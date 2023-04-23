cfd36aee4a8c815d9e201c249166f45b3ced738d
Iván Galán <92123743+ivansito4@users.noreply.github.com>
2023-04-23 11:18:54 +0200

    Agregar archivos de proyecto.

---
+++ EgoPadel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EgoPadel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
