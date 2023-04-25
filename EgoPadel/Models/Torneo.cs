786206f336827cbf6e0b7590b04e46298d94365e
Álvaro Cano Sánchez <acanito2003@gmail.com>
2023-04-25 19:01:29 +0200

    Torneo hecho

---
+++ EgoPadel/Models/Torneo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EgoPadel.Models
+{
+    public class Torneo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tipo del Torneo obligatorio.")]
+        public byte Tipo { get; set; }
+
+        [Required(ErrorMessage = "Fecha del Torneo obligatoria.")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Número de plazas del Torneo obligatoria.")]
+        public int NroPlazas { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Premio { get; set; }
+    }
+}
