b01e647300b0a6ed97c15db7b5db9fd4953ccc06
Iván Galán <92123743+ivansito4@users.noreply.github.com>
2023-04-27 20:24:19 +0200

    Vista crear y mostrar

--- EgoPadel/Controllers/ProductoController.cs
+++ EgoPadel/Controllers/ProductoController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EgoPadel.Datos;
+using EgoPadel.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EgoPadel.Controllers
 {
     public class ProductoController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ProductoController(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
         public IActionResult Index()
         {
+            IEnumerable<Producto> listaProducto = _db.Producto;
+            return View(listaProducto);
+        }
+
+        //Get
+        public IActionResult Crear()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Producto productoCreado)
+        {
+            _db.Producto.Add(productoCreado);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index)); //Para que mande a index al hacer submit
         }
     }
 }
