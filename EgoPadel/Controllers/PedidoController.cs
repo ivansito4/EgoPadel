ed393619a36f470ead396f2d6fcc824083cb7f19
Álvaro Cano Sánchez <acanito2003@gmail.com>
2023-04-27 23:15:40 +0200

    Macho

---
+++ EgoPadel/Controllers/PedidoController.cs
@@ -0,0 +1,36 @@
+﻿using EgoPadel.Datos;
+using EgoPadel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgoPadel.Controllers
+{
+    public class PedidoController : Controller   
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PedidoController(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Pedido> listaPedido = _db.Pedido;
+            return View(listaPedido);
+        }
+
+        //Get
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Pedido pedido)
+        {
+            _db.Pedido.Add(pedido);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index)); //Para que mande a index al hacer submit
+        }
+    }
+}
