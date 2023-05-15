1944d5d8c471c972ec71f5da836ffb3a85a014ce
Álvaro Cano Sánchez <acanito2003@gmail.com>
2023-05-15 17:51:51 +0200

    Views Y Controllers

---
+++ EgoPadel/Controllers/XProductoController.cs
@@ -0,0 +1,21 @@
+﻿using EgoPadel.Datos;
+using EgoPadel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgoPadel.Controllers
+{
+    public class XProductoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public XProductoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Producto> listaProducto = _db.Producto;
+            return View(listaProducto);
+        }
+    }
+}
---
+++ EgoPadel/Controllers/XReservaController.cs
@@ -0,0 +1,41 @@
+﻿using EgoPadel.Datos;
+using EgoPadel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgoPadel.Controllers
+{
+    public class XReservaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public XReservaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ReservaPista> listaPista = _db.ReservaPista;
+            return View(listaPista);
+        }
+
+        //Get
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(ReservaPista reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ReservaPista.Add(reserva);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index)); //Para que mande a index al hacer submit
+            }
+            return View(reserva);
+
+        }
+    }
+}
