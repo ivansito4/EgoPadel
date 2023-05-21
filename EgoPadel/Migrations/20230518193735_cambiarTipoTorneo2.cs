a573553c6358621cf8c9213f69d0d05797196348
Iván Galán <92123743+ivansito4@users.noreply.github.com>
2023-05-21 11:08:24 +0200

    Cambios vista torneo

---
+++ EgoPadel/Migrations/20230518193735_cambiarTipoTorneo2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EgoPadel.Migrations
+{
+    /// <inheritdoc />
+    public partial class cambiarTipoTorneo2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "Torneo",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Tipo",
+                table: "Torneo",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
