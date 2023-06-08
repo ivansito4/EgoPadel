e76a490badd4e6e379e411e7741775d7c5730dfc
Iván Galán <92123743+ivansito4@users.noreply.github.com>
2023-06-08 10:38:41 +0200

    Cambios footer y añadir ultimas paginas

---
+++ EgoPadel/Controllers/GeneralController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EgoPadel.Controllers
+{
+	public class GeneralController : Controller
+	{
+		public IActionResult MapaWeb()
+		{
+			return View();
+		}
+		
+		public IActionResult Accesibilidad()
+		{
+			return View();
+		}
+
+		public IActionResult Contacto()
+		{
+			return View();
+		}
+	}
+}
